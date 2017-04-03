8e95d0eb112ed28392b2f2c4d49ea8cbff1b6508
ranajoy-dutta <ranajoydutta7@gmail.com>
2017-04-03 22:35:38 +0530

    ADDITION OF TWO NUMBERS

---
+++ ADDITION.C
@@ -0,0 +1,13 @@
+/* ADDITION OF TWO NUMBERS */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,add;    //VARIABLE DECLARATION
+	clrscr();
+	printf("Enter the two numbers :");
+	scanf("%d%d",&a,&b);  //INPUT
+	add=a+b;              //ADDING
+	printf("Sum of given numbers = %d",add); //OUTPUT
+	getch();
+}
