27f10410e16946c379723fe788f10c9f07d80a7f
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-08-19 15:41:50 +0530

    Addition of two integers

---
+++ 1. SUM.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num1, num2;
+	clrscr();
+	printf("Enter Two Integer numbers :: ");
+	scanf("%d %d", &num1, &num2);
+	printf("\nSum = %d", num1+num2);
+	getch();
+	return 0;
+}
